b307a128498d0c41adda92b3f97d50fc246ed52c
Rilly V <rillyv.dev@gmail.com>
2023-05-03 16:42:48 +0200

    Fixed migrate

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -12,8 +12,8 @@ else
     echo "Using existing .env file"
 fi
 
-php artisan migrate
 php artisan key:generate
+php artisan migrate
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
