0005ee92ffcd51593f997e1a01209116126e4113
Rilly V <rillyv.dev@gmail.com>
2023-05-03 22:08:57 +0200

    Fixed database migration

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,29 +5,11 @@ if [ ! -f vendor/autoload.php ]; then
     composer install --no-progress --no-interaction
 fi
 
-if [ ! -f .env ]; then
-    echo "Creating .env file for env $APP_ENV"
-    cp .env.example .env
-    php artisan key:generate
-else
-    echo "Using existing .env file"
-fi
-
-role=${CONTAINER_ROLE:-app}
-
-if [ "$role" = "app" ]; then
-    php artisan migrate
-    php artisan key:generate
-    php artisan cache:clear
-    php artisan config:clear
-    php artisan route:clear
-    php artisan app:collect-data
-    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
-    exec docker-php-entrypoint "$@"
-elif [ "$role" = "queue" ]; then
-    echo "Running the queue ... "
-    php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
-elif [ "$role" = "websocket" ]; then
-    echo "Running the websocket server ... "
-    php artisan websockets:serve
-fi
+php artisan key:generate
+php artisan migrate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan app:collect-data
+php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+exec docker-php-entrypoint "$@"
